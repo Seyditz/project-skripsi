6cd98c395074abcd5574d2a57f702cb2edc895fc
Chordan <chordan345@gmail.com>
2024-05-20 15:10:08 +0700

    saddasasd

--- main.go
+++ main.go
@@ -37,7 +37,7 @@ func main() {
 
 	r := gin.Default()
 
-	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	// r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"message": "Hello"})
