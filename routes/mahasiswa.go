b176e6590d15bacce99b111df1046047fb0ffd20
Seyditz <chordan345@gmail.com>
2024-06-14 15:47:08 +0700

    mahasiswa route auth disable

--- routes/mahasiswa.go
+++ routes/mahasiswa.go
@@ -2,16 +2,23 @@ package routes
 
 import (
 	"github.com/Seyditz/project-skripsi/controllers"
-	middleware "github.com/Seyditz/project-skripsi/middlewares"
-	"github.com/Seyditz/project-skripsi/utils"
 	"github.com/gin-gonic/gin"
 )
 
+// func MahasiswaRoutes(r *gin.Engine) {
+// 	mahasiswaRoutes := r.Group("/mahasiswa", middleware.AuthMiddleware())
+// 	mahasiswaRoutes.GET("/", utils.Authorize("mahasiswa"), controllers.GetAllMahasiswa)
+// 	mahasiswaRoutes.GET("/:id", utils.Authorize("mahasiswa"), controllers.GetMahasiswaByID)
+// 	mahasiswaRoutes.POST("/", controllers.CreateMahasiswa)
+// 	mahasiswaRoutes.DELETE("/:id", utils.Authorize("mahasiswa"), controllers.DeleteMahasiswa)
+// 	mahasiswaRoutes.PUT("/:id", utils.Authorize("mahasiswa"), controllers.UpdateMahasiswa)
+// }
+
 func MahasiswaRoutes(r *gin.Engine) {
-	mahasiswaRoutes := r.Group("/mahasiswa", middleware.AuthMiddleware())
-	mahasiswaRoutes.GET("/", utils.Authorize("mahasiswa"), controllers.GetAllMahasiswa)
-	mahasiswaRoutes.GET("/:id", utils.Authorize("mahasiswa"), controllers.GetMahasiswaByID)
+	mahasiswaRoutes := r.Group("/mahasiswa")
+	mahasiswaRoutes.GET("/", controllers.GetAllMahasiswa)
+	mahasiswaRoutes.GET("/:id", controllers.GetMahasiswaByID)
 	mahasiswaRoutes.POST("/", controllers.CreateMahasiswa)
-	mahasiswaRoutes.DELETE("/:id", utils.Authorize("mahasiswa"), controllers.DeleteMahasiswa)
-	mahasiswaRoutes.PUT("/:id", utils.Authorize("mahasiswa"), controllers.UpdateMahasiswa)
+	mahasiswaRoutes.DELETE("/:id", controllers.DeleteMahasiswa)
+	mahasiswaRoutes.PUT("/:id", controllers.UpdateMahasiswa)
 }
