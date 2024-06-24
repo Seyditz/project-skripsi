638ea9c75cb3a58c9cdd8fa7b3c1913c39d93697
Seyditz <chordan345@gmail.com>
2024-06-24 22:01:42 +0700

    db migration

--- database/db.go
+++ database/db.go
@@ -4,7 +4,7 @@ import (
 	// "github.com/Seyditz/project-skripsi/models"
 
 	// "github.com/Seyditz/project-skripsi/models"
-	// "github.com/Seyditz/project-skripsi/models"
+	"github.com/Seyditz/project-skripsi/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -12,17 +12,17 @@ import (
 var DB *gorm.DB
 
 func Connect() {
-	dsn := "postgres://koyeb-adm:5Ki1EOJRaDhj@ep-floral-dream-a2znj19n.eu-central-1.pg.koyeb.app/koyebdb"
+	dsn := "postgres://koyeb-adm:Csz1XbkK4gDU@ep-withered-shape-a29hnvoi.eu-central-1.pg.koyeb.app/koyebdb"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
 
-	// db.AutoMigrate(&models.Admin{})
-	// db.AutoMigrate(&models.Dosen{})
-	// db.AutoMigrate(&models.Mahasiswa{})
-	// db.AutoMigrate(&models.Pengajuan{})
-	// db.AutoMigrate(&models.Judul{})
-	// db.AutoMigrate(&models.MobileNotification{})
+	db.AutoMigrate(&models.Admin{})
+	db.AutoMigrate(&models.Dosen{})
+	db.AutoMigrate(&models.Mahasiswa{})
+	db.AutoMigrate(&models.Pengajuan{})
+	db.AutoMigrate(&models.Judul{})
+	db.AutoMigrate(&models.MobileNotification{})
 	DB = db
 }
